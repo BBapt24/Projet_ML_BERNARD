3492bbdd960b5de849c096c6669ca5905062da50
BBapt24 <150921474+BBapt24@users.noreply.github.com>
2023-12-18 18:17:33 +0100

    Update resnet.py

--- resnet.py
+++ resnet.py
@@ -28,108 +28,122 @@ import time
 
 
 
-class ResNet(nn.Module):
-    def __init__(self, config, output_dim):
+
+
+class ResBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, downsample):
         super().__init__()
-                
-        block, n_blocks, channels = config
-        self.in_channels = channels[0]
-            
-        assert len(n_blocks) == len(channels) == 4
-        
-        self.conv1 = nn.Conv2d(3, self.in_channels, kernel_size = 7, stride = 2, padding = 3, bias = False)
-        self.bn1 = nn.BatchNorm2d(self.in_channels)
-        self.relu = nn.ReLU(inplace = True)
-        self.maxpool = nn.MaxPool2d(kernel_size = 3, stride = 2, padding = 1)
-        
-        self.layer1 = self.get_resnet_layer(block, n_blocks[0], channels[0])
-        self.layer2 = self.get_resnet_layer(block, n_blocks[1], channels[1], stride = 2)
-        self.layer3 = self.get_resnet_layer(block, n_blocks[2], channels[2], stride = 2)
-        self.layer4 = self.get_resnet_layer(block, n_blocks[3], channels[3], stride = 2)
-        
-        self.avgpool = nn.AdaptiveAvgPool2d((1,1))
-        self.fc = nn.Linear(self.in_channels, output_dim)
-        
-    def get_resnet_layer(self, block, n_blocks, channels, stride = 1):
-    
-        layers = []
-        
-        if self.in_channels != block.expansion * channels:
-            downsample = True
-        else:
-            downsample = False
-        
-        layers.append(block(self.in_channels, channels, stride, downsample))
-        
-        for i in range(1, n_blocks):
-            layers.append(block(block.expansion * channels, channels))
-
-        self.in_channels = block.expansion * channels
-            
-        return nn.Sequential(*layers)
-        
-    def forward(self, x):
-        
-        x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.relu(x)
-        x = self.maxpool(x)
-
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-        x = self.layer4(x)
-        
-        x = self.avgpool(x)
-        h = x.view(x.shape[0], -1)
-        x = self.fc(h)
-        
-        return x, h
-
-
-class BasicBlock(nn.Module):
-    
-    expansion = 1
-    
-    def __init__(self, in_channels, out_channels, stride = 1, downsample = False):
-        super().__init__()
-                
-        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size = 3, 
-                               stride = stride, padding = 1, bias = False)
-        self.bn1 = nn.BatchNorm2d(out_channels)
-        
-        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size = 3, 
-                               stride = 1, padding = 1, bias = False)
-        self.bn2 = nn.BatchNorm2d(out_channels)
-        
-        self.relu = nn.ReLU(inplace = True)
-        
         if downsample:
-            conv = nn.Conv2d(in_channels, out_channels, kernel_size = 1, 
-                             stride = stride, bias = False)
-            bn = nn.BatchNorm2d(out_channels)
-            downsample = nn.Sequential(conv, bn)
+            self.conv1 = nn.Conv2d(
+                in_channels, out_channels, kernel_size=3, stride=2, padding=1)
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=2),
+                nn.BatchNorm2d(out_channels)
+            )
         else:
-            downsample = None
-        
+            self.conv1 = nn.Conv2d(
+                in_channels, out_channels, kernel_size=3, stride=1, padding=1)
+            self.shortcut = nn.Sequential()
+
+        self.conv2 = nn.Conv2d(out_channels, out_channels,
+                               kernel_size=3, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+
+    def forward(self, input):
+        shortcut = self.shortcut(input)
+        input = nn.ReLU()(self.bn1(self.conv1(input)))
+        input = nn.ReLU()(self.bn2(self.conv2(input)))
+        input = input + shortcut
+        return nn.ReLU()(input)
+
+
+
+#On n'utilise pas le Bottleneck ici puisqu'il s'agit d'une architecture ResNet18
+class ResBottleneckBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, downsample):
+        super().__init__()
         self.downsample = downsample
-        
-    def forward(self, x):
-        
-        i = x
-        
-        x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.relu(x)
-        
-        x = self.conv2(x)
-        x = self.bn2(x)
-        
-        if self.downsample is not None:
-            i = self.downsample(i)
-                        
-        x += i
-        x = self.relu(x)
-        
-        return x
+        self.conv1 = nn.Conv2d(in_channels, out_channels//4,
+                               kernel_size=1, stride=1)
+        self.conv2 = nn.Conv2d(
+            out_channels//4, out_channels//4, kernel_size=3, stride=2 if downsample else 1, padding=1)
+        self.conv3 = nn.Conv2d(out_channels//4, out_channels, kernel_size=1, stride=1)
+
+        if self.downsample or in_channels != out_channels:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, kernel_size=1,
+                          stride=2 if self.downsample else 1),
+                nn.BatchNorm2d(out_channels)
+            )
+        else:
+            self.shortcut = nn.Sequential()
+
+        self.bn1 = nn.BatchNorm2d(out_channels//4)
+        self.bn2 = nn.BatchNorm2d(out_channels//4)
+        self.bn3 = nn.BatchNorm2d(out_channels)
+
+    def forward(self, input):
+        shortcut = self.shortcut(input)
+        input = nn.ReLU()(self.bn1(self.conv1(input)))
+        input = nn.ReLU()(self.bn2(self.conv2(input)))
+        input = nn.ReLU()(self.bn3(self.conv3(input)))
+        input = input + shortcut
+        return nn.ReLU()(input)
+
+class ResNet(nn.Module):
+    def __init__(self, in_channels, resblock, repeat, useBottleneck=False, outputs=1000):
+        super().__init__()
+        self.layer0 = nn.Sequential(
+            nn.Conv2d(in_channels, 64, kernel_size=7, stride=2, padding=3),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU()
+        )
+
+        if useBottleneck:
+            filters = [64, 256, 512, 1024, 2048]
+        else:
+            filters = [64, 64, 128, 256, 512]
+
+        self.layer1 = nn.Sequential()
+        self.layer1.add_module('conv2_1', resblock(filters[0], filters[1], downsample=False))
+        for i in range(1, repeat[0]):
+                self.layer1.add_module('conv2_%d'%(i+1,), resblock(filters[1], filters[1], downsample=False))
+
+        self.layer2 = nn.Sequential()
+        self.layer2.add_module('conv3_1', resblock(filters[1], filters[2], downsample=True))
+        for i in range(1, repeat[1]):
+                self.layer2.add_module('conv3_%d' % (
+                    i+1,), resblock(filters[2], filters[2], downsample=False))
+
+        self.layer3 = nn.Sequential()
+        self.layer3.add_module('conv4_1', resblock(filters[2], filters[3], downsample=True))
+        for i in range(1, repeat[2]):
+            self.layer3.add_module('conv2_%d' % (
+                i+1,), resblock(filters[3], filters[3], downsample=False))
+
+        self.layer4 = nn.Sequential()
+        self.layer4.add_module('conv5_1', resblock(filters[3], filters[4], downsample=True))
+        for i in range(1, repeat[3]):
+            self.layer4.add_module('conv3_%d'%(i+1,),resblock(filters[4], filters[4], downsample=False))
+
+        self.gap = torch.nn.AdaptiveAvgPool2d(1)
+        self.fc = torch.nn.Linear(filters[4], outputs)
+
+    def forward(self, input):
+        input = self.layer0(input)
+        input = self.layer1(input)
+        input = self.layer2(input)
+        input = self.layer3(input)
+        input = self.layer4(input)
+        input = self.gap(input)
+        input = torch.flatten(input, start_dim=1)
+        input = self.fc(input)
+
+        return input
+
+resnet18 = ResNet(3, ResBlock, [2, 2, 2, 2], useBottleneck=False, outputs=1000)
+resnet18.to(torch.device("cuda:0" if torch.cuda.is_available() else "cpu"))
+summary(resnet18, (3, 224, 224))
 
