404b3822f1c4699aef391275ca40184f605f7f33
BBapt24 <150921474+BBapt24@users.noreply.github.com>
2023-12-18 18:41:01 +0100

    Update tranf_model.py

--- tranf_model.py
+++ tranf_model.py
@@ -1,5 +1,28 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torch.optim.lr_scheduler as lr_scheduler
+from torch.optim.lr_scheduler import _LRScheduler
+import torch.utils.data as data
 
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+import torchvision.models as models
 
+from sklearn import decomposition
+from sklearn import manifold
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import ConfusionMatrixDisplay
+import matplotlib.pyplot as plt
+import numpy as np
+
+import copy
+from collections import namedtuple
+import os
+import random
+import shutil
+import time
 
 
 
@@ -56,7 +79,6 @@ class MLPBlock(nn.Module):
 
 
 class TransformerEncoderBlock(nn.Module):
-    """Creates a Transformer Encoder block."""
 
     def __init__(self,
                  embedding_dim:int=768, 
@@ -81,3 +103,58 @@ class TransformerEncoderBlock(nn.Module):
         
         return x
     
+class ViT(nn.Module):
+  
+    def __init__(self,
+                 img_size:int=224, 
+                 in_channels:int=3,
+                 patch_size:int=16, 
+                 num_transformer_layers:int=12, 
+                 embedding_dim:int=768, 
+                 mlp_size:int=3072, 
+                 num_heads:int=12, 
+                 attn_dropout:float=0,
+                 mlp_dropout:float=0.1, 
+                 embedding_dropout:float=0.1,
+                 num_classes:int=1000): 
+        super().__init__() 
+        
+        assert img_size % patch_size == 0, f"Image size must be divisible by patch size, image size: {img_size}, patch size: {patch_size}."
+        
+        self.num_patches = (img_size * img_size) // patch_size**2
+                 
+        self.class_embedding = nn.Parameter(data=torch.randn(1, 1, embedding_dim),
+                                            requires_grad=True)
+        
+        self.position_embedding = nn.Parameter(data=torch.randn(1, self.num_patches+1, embedding_dim),
+                                               requires_grad=True)
+                
+        self.embedding_dropout = nn.Dropout(p=embedding_dropout)
+        
+        self.patch_embedding = PatchEmbedding(in_channels=in_channels,
+                                              patch_size=patch_size,
+                                              embedding_dim=embedding_dim)
+        
+        self.transformer_encoder = nn.Sequential(*[TransformerEncoderBlock(embedding_dim=embedding_dim,
+                                                                            num_heads=num_heads,
+                                                                            mlp_size=mlp_size,
+                                                                            mlp_dropout=mlp_dropout) for _ in range(num_transformer_layers)])
+       
+        self.classifier = nn.Sequential(
+            nn.LayerNorm(normalized_shape=embedding_dim),
+            nn.Linear(in_features=embedding_dim, 
+                      out_features=num_classes)
+        )
+    
+    def forward(self, x):
+        
+        batch_size = x.shape[0]
+        class_token = self.class_embedding.expand(batch_size, -1, -1) 
+        x = self.patch_embedding(x)
+        x = torch.cat((class_token, x), dim=1)
+        x = self.position_embedding + x
+        x = self.embedding_dropout(x)
+        x = self.transformer_encoder(x)
+        x = self.classifier(x[:, 0]) 
+
+        return x       
