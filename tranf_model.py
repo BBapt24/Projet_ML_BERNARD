4fbdd0f1f02dba7a0a3d8502b0cf5f929e2c77c6
BBapt24 <150921474+BBapt24@users.noreply.github.com>
2023-12-18 18:35:21 +0100

    Update tranf_model.py

--- tranf_model.py
+++ tranf_model.py
@@ -1,1 +1,83 @@
 
+
+
+
+
+
+
+class MultiheadSelfAttentionBlock(nn.Module):
+  
+    def __init__(self,
+                 embedding_dim:int=768, 
+                 num_heads:int=12, 
+                 attn_dropout:float=0): 
+        super().__init__()
+        
+        self.layer_norm = nn.LayerNorm(normalized_shape=embedding_dim)
+        
+        self.multihead_attn = nn.MultiheadAttention(embed_dim=embedding_dim,
+                                                    num_heads=num_heads,
+                                                    dropout=attn_dropout,
+                                                    batch_first=True) 
+
+    def forward(self, x):
+        x = self.layer_norm(x)
+        attn_output, _ = self.multihead_attn(query=x, 
+                                             key=x, 
+                                             value=x, 
+                                             need_weights=False) 
+        return attn_output
+
+
+class MLPBlock(nn.Module):
+
+    def __init__(self,
+                 embedding_dim:int=768, 
+                 mlp_size:int=3072, 
+                 dropout:float=0.1): 
+        super().__init__()
+
+        self.layer_norm = nn.LayerNorm(normalized_shape=embedding_dim)
+
+        self.mlp = nn.Sequential(
+            nn.Linear(in_features=embedding_dim,
+                      out_features=mlp_size),
+            nn.GELU(), 
+            nn.Dropout(p=dropout),
+            nn.Linear(in_features=mlp_size, 
+                      out_features=embedding_dim), 
+            nn.Dropout(p=dropout)
+        )
+
+    def forward(self, x):
+        x = self.layer_norm(x)
+        x = self.mlp(x)
+        return x
+
+
+class TransformerEncoderBlock(nn.Module):
+    """Creates a Transformer Encoder block."""
+
+    def __init__(self,
+                 embedding_dim:int=768, 
+                 num_heads:int=12, 
+                 mlp_size:int=3072, 
+                 mlp_dropout:float=0.1, 
+                 attn_dropout:float=0): 
+        super().__init__()
+                   
+        self.msa_block = MultiheadSelfAttentionBlock(embedding_dim=embedding_dim,
+                                                     num_heads=num_heads,
+                                                     attn_dropout=attn_dropout)
+
+        self.mlp_block =  MLPBlock(embedding_dim=embedding_dim,
+                                   mlp_size=mlp_size,
+                                   dropout=mlp_dropout)
+         
+    def forward(self, x):
+        
+        x =  self.msa_block(x) + x 
+        x = self.mlp_block(x) + x 
+        
+        return x
+    
